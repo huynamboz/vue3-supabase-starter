60edb870ba6eff4397c6c41e9749e6fd895ce18b
huynamboz <teahuynam2003@gmail.com>
2024-03-19 15:05:18 +0700

    chore: remove supabase

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import { supabase } from './supabase'
 
-const session = ref()
-
-onMounted(() => {
-  supabase.auth.getSession().then(({ data }) => {
-    session.value = data.session
-  })
-
-  supabase.auth.onAuthStateChange((_, _session) => {
-    session.value = _session
-  })
-})
 </script>
 
 <template>
